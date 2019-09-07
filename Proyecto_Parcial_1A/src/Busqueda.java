f4a1ee4412eb72fb6b514aa845a09434c5012fff
Martin0698 <A01632780@itesm.mx>
2019-09-06 23:56:16 -0500

    Nuevas Clases

---
+++ Proyecto_Parcial_1A/src/Busqueda.java
@@ -0,0 +1,11 @@
+
+public class Busqueda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	
+}
---
+++ Proyecto_Parcial_1A/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+	
+	static String[] expresion;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		expresion= args;
+		
+		Calculadora.postfijo(expresion);
+		
+	}
+
+}
